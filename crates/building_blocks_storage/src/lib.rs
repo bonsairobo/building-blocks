e0b73989225a9fd7747b6e0af1bf15e247cb1e05
iMplode nZ <rg@youxplode.com>
2020-12-19 19:46:57 -0800

    Exported get_ref.

--- crates/building_blocks_storage/src/lib.rs
+++ crates/building_blocks_storage/src/lib.rs
@@ -43,9 +43,9 @@ pub mod prelude {
         ChunkMapBuilder2, ChunkMapBuilder3, ChunkReadStorage, ChunkWriteStorage, Compressed,
         CompressibleChunkMap, CompressibleChunkMapReader, CompressibleChunkStorage,
         CompressibleChunkStorageReader, Compression, FastArrayCompression, FastChunkCompression,
-        ForEach, ForEachMut, Get, GetMut, IsEmpty, IterChunkKeys, Local, LocalChunkCache,
-        LocalChunkCache2, LocalChunkCache3, OctreeSet, ReadExtent, SerializableChunkMap, Stride,
-        TransformMap, WriteExtent,
+        ForEach, ForEachMut, ForEachRef, Get, GetMut, GetRef, IsEmpty, IterChunkKeys, Local,
+        LocalChunkCache, LocalChunkCache2, LocalChunkCache3, OctreeSet, ReadExtent,
+        SerializableChunkMap, Stride, TransformMap, WriteExtent,
     };
 
     #[cfg(feature = "lz4")]

67f6dad5c195f8bdbd55369610c03f155fd8ec32
Duncan <bonsairobo@gmail.com>
2021-02-02 20:18:36 -0800

    set default metadata type for Chunk and ChunkStorage

--- crates/building_blocks_storage/src/chunk_map/chunk.rs
+++ crates/building_blocks_storage/src/chunk_map/chunk.rs
@@ -7,15 +7,15 @@ use serde::{Deserialize, Serialize};
 
 /// One piece of a chunked lattice map. Contains both some generic metadata and the data for each point in the chunk extent.
 #[derive(Clone, Deserialize, Serialize)]
-pub struct Chunk<N, T, Meta> {
+pub struct Chunk<N, T, Meta = ()> {
     pub metadata: Meta,
     pub array: ArrayN<N, T>,
 }
 
 /// A 2-dimensional `Chunk`.
-pub type Chunk2<T, Meta> = Chunk<[i32; 2], T, Meta>;
+pub type Chunk2<T, Meta = ()> = Chunk<[i32; 2], T, Meta>;
 /// A 3-dimensional `Chunk`.
-pub type Chunk3<T, Meta> = Chunk<[i32; 3], T, Meta>;
+pub type Chunk3<T, Meta = ()> = Chunk<[i32; 3], T, Meta>;
 
 impl<N, T> Chunk<N, T, ()> {
     /// Constructs a chunk without metadata.

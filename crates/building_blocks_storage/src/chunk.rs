f894faa75c72cfb81727a01c4e663e4208d517a1
Duncan <bonsairobo@gmail.com>
2021-03-10 22:14:19 -0800

    Revert "remove ChunkWithMeta, since it would require a ChunkMapBuilder to be useful, and it's something that should just be implemented by the user"
    This reverts commit 2faecac7b7313fcb69418c9921cfd229c351d5b6.

--- crates/building_blocks_storage/src/chunk.rs
+++ crates/building_blocks_storage/src/chunk.rs
@@ -4,6 +4,8 @@ pub use serialization::*;
 
 use crate::ArrayNx1;
 
+use building_blocks_core::prelude::*;
+
 /// One piece of a chunked lattice map.
 pub trait Chunk {
     /// The inner array type. This makes it easier for `Chunk` implementations to satisfy access trait bounds by inheriting them
@@ -28,5 +30,29 @@ impl<N, T> Chunk for ArrayNx1<N, T> {
     #[inline]
     fn array_mut(&mut self) -> &mut Self::Array {
         self
+    }
+}
+
+pub struct ChunkWithMeta<N, T, Meta> {
+    pub array: ArrayNx1<N, T>,
+    pub metadata: Meta,
+}
+
+impl<N, T, Meta> Chunk for ChunkWithMeta<N, T, Meta>
+where
+    PointN<N>: IntegerPoint<N>,
+    T: Clone + Default,
+    Meta: Default,
+{
+    type Array = ArrayNx1<N, T>;
+
+    #[inline]
+    fn array_ref(&self) -> &Self::Array {
+        &self.array
+    }
+
+    #[inline]
+    fn array_mut(&mut self) -> &mut Self::Array {
+        &mut self.array
     }
 }

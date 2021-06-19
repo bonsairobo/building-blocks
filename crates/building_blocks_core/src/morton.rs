22cefae2c0a8e583f79302197a9ad23670c9bca9
Duncan <bonsairobo@gmail.com>
2021-06-19 14:49:15 -0700

    use u128 for Morton3 instead of u64; while bitintr does not support u128, we just work around it by concatenating two u64s

--- crates/building_blocks_core/src/morton.rs
+++ crates/building_blocks_core/src/morton.rs
@@ -10,37 +10,16 @@ use std::fmt;
 // ███████╗██████╔╝
 // ╚══════╝╚═════╝
 
-/// A Morton-encoded `Point2i`.
-///
-/// Since the encoding packs bits into a u64, and we have two i32s in a `Point2i`, all points can be encoded.
+/// A Morton-encoded `Point2i`. Uses a `u64` to support the full set of `Point3i`s.
 ///
 /// https://en.wikipedia.org/wiki/Z-order_curve
 #[derive(Clone, Copy, Eq, Ord, PartialEq, PartialOrd)]
 pub struct Morton2(pub u64);
 
 impl Morton2 {
-    // To pack 2 ints into a u64, we can use at most 32 bits per int.
-    const MIN_SUPPORTED_INT: i32 = i32::MIN;
-    const MAX_SUPPORTED_INT: i32 = i32::MAX;
-
-    // Only 21 bits can be set in each mask.
+    // Only 32 bits can be set in each mask.
     const X_MASK: u64 = 0b01010101_01010101_01010101_01010101_01010101_01010101_01010101_01010101;
     const Y_MASK: u64 = 0b10101010_10101010_10101010_10101010_10101010_10101010_10101010_10101010;
-
-    /// Send the supported range of i32 into the lower 21 bits of a u64 while preserving the total order.
-    #[inline]
-    fn translate(x: i32) -> u64 {
-        debug_assert!(x >= Self::MIN_SUPPORTED_INT);
-        debug_assert!(x <= Self::MAX_SUPPORTED_INT);
-
-        x.wrapping_sub(Self::MIN_SUPPORTED_INT) as u64
-    }
-
-    /// The inverse of `translate`.
-    #[inline]
-    fn untranslate(x: u64) -> i32 {
-        (x as i32).wrapping_add(Self::MIN_SUPPORTED_INT)
-    }
 }
 
 impl fmt::Debug for Morton2 {
@@ -52,7 +31,7 @@ impl fmt::Debug for Morton2 {
 impl From<Point2i> for Morton2 {
     #[inline]
     fn from(p: Point2i) -> Self {
-        Self(Self::translate(p.x()).pdep(Self::X_MASK) | Self::translate(p.y()).pdep(Self::Y_MASK))
+        Self(translate(p.x()).pdep(Self::X_MASK) | translate(p.y()).pdep(Self::Y_MASK))
     }
 }
 
@@ -60,8 +39,8 @@ impl From<Morton2> for Point2i {
     #[inline]
     fn from(m: Morton2) -> Self {
         Self([
-            Morton2::untranslate(m.0.pext(Morton2::X_MASK)),
-            Morton2::untranslate(m.0.pext(Morton2::Y_MASK)),
+            untranslate(m.0.pext(Morton2::X_MASK)),
+            untranslate(m.0.pext(Morton2::Y_MASK)),
         ])
     }
 }
@@ -73,15 +52,11 @@ impl From<Morton2> for Point2i {
 // ██████╔╝██████╔╝
 // ╚═════╝ ╚═════╝
 
-/// A Morton-encoded `Point3i`.
-///
-/// Since the encoding packs bits into a u64, and we have three i32s in a `Point3i`, only 21 bits may be used per component.
-/// This imposes a range on the components of `[Self::MIN_SUPPORTED_INT, Self::MAX_SUPPORTED_INT]`. Falling out of range will
-/// cause a panic in debug mode only.
+/// A Morton-encoded `Point3i`. Uses a `u128` to support the full set of `Point3i`s.
 ///
 /// https://en.wikipedia.org/wiki/Z-order_curve
 #[derive(Clone, Copy, Eq, Ord, PartialEq, PartialOrd)]
-pub struct Morton3(pub u64);
+pub struct Morton3(pub u128);
 
 impl fmt::Debug for Morton3 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
@@ -89,55 +64,80 @@ impl fmt::Debug for Morton3 {
     }
 }
 
+// bitintr does not support u128, so we need to use two separate u64s and concatenate them.
 impl Morton3 {
-    // To pack 3 ints into a u64, we can use at most 21 bits per int.
-    pub const MIN_SUPPORTED_INT: i32 = -(1 << 20);
-    pub const MAX_SUPPORTED_INT: i32 = (1 << 20) - 1;
-
     // Only 21 bits can be set in each mask.
-    pub const X_MASK: u64 =
-        0b00010010_01001001_00100100_10010010_01001001_00100100_10010010_01001001;
-    pub const Y_MASK: u64 =
-        0b00100100_10010010_01001001_00100100_10010010_01001001_00100100_10010010;
-    pub const Z_MASK: u64 =
-        0b01001001_00100100_10010010_01001001_00100100_10010010_01001001_00100100;
-
-    /// Send the supported range of i32 into the lower 21 bits of a u64 while preserving the total order.
-    #[inline]
-    fn translate(x: i32) -> u64 {
-        debug_assert!(x >= Morton3::MIN_SUPPORTED_INT);
-        debug_assert!(x <= Morton3::MAX_SUPPORTED_INT);
-
-        x.wrapping_sub(Morton3::MIN_SUPPORTED_INT) as u64
-    }
-
-    /// The inverse of `translate`.
-    #[inline]
-    fn untranslate(x: u64) -> i32 {
-        (x as i32).wrapping_add(Morton3::MIN_SUPPORTED_INT)
-    }
+    const X_MASK: u64 = 0b00010010_01001001_00100100_10010010_01001001_00100100_10010010_01001001;
+    const Y_MASK: u64 = 0b00100100_10010010_01001001_00100100_10010010_01001001_00100100_10010010;
+    const Z_MASK: u64 = 0b01001001_00100100_10010010_01001001_00100100_10010010_01001001_00100100;
 }
 
 impl From<Point3i> for Morton3 {
     #[inline]
     fn from(p: Point3i) -> Self {
-        Self(
-            Self::translate(p.x()).pdep(Morton3::X_MASK)
-                | Self::translate(p.y()).pdep(Morton3::Y_MASK)
-                | Self::translate(p.z()).pdep(Morton3::Z_MASK),
-        )
+        let x_t = translate(p.x());
+        let y_t = translate(p.y());
+        let z_t = translate(p.z());
+
+        // PERF: might be better to use BEXTR instead of masking and shifting
+        const LOW_21: u64 = (1 << 21) - 1;
+        const HIGH_11: u64 = ((1 << 11) - 1) << 21;
+
+        let x_t_low_21 = x_t & LOW_21;
+        let y_t_low_21 = y_t & LOW_21;
+        let z_t_low_21 = z_t & LOW_21;
+
+        let morton_low_63 = x_t_low_21.pdep(Morton3::X_MASK)
+            | y_t_low_21.pdep(Morton3::Y_MASK)
+            | z_t_low_21.pdep(Morton3::Z_MASK);
+
+        let x_t_high_11 = (x_t & HIGH_11) >> 21;
+        let y_t_high_11 = (y_t & HIGH_11) >> 21;
+        let z_t_high_11 = (z_t & HIGH_11) >> 21;
+
+        let morton_high_33 = x_t_high_11.pdep(Morton3::X_MASK)
+            | y_t_high_11.pdep(Morton3::Y_MASK)
+            | z_t_high_11.pdep(Morton3::Z_MASK);
+
+        Self(((morton_high_33 as u128) << 63) | morton_low_63 as u128)
     }
 }
 
 impl From<Morton3> for Point3i {
     #[inline]
     fn from(m: Morton3) -> Self {
+        const LOW_63: u128 = (1 << 63) - 1;
+        const HIGH_33: u128 = ((1 << 33) - 1) << 63;
+
+        let m_low_63 = (m.0 & LOW_63) as u64;
+        let m_high_33 = ((m.0 & HIGH_33) >> 63) as u64;
+
+        let x_t_low_21 = m_low_63.pext(Morton3::X_MASK);
+        let y_t_low_21 = m_low_63.pext(Morton3::Y_MASK);
+        let z_t_low_21 = m_low_63.pext(Morton3::Z_MASK);
+
+        let x_t_high_11 = m_high_33.pext(Morton3::X_MASK) << 21;
+        let y_t_high_11 = m_high_33.pext(Morton3::Y_MASK) << 21;
+        let z_t_high_11 = m_high_33.pext(Morton3::Z_MASK) << 21;
+
         Self([
-            Morton3::untranslate(m.0.pext(Morton3::X_MASK)),
-            Morton3::untranslate(m.0.pext(Morton3::Y_MASK)),
-            Morton3::untranslate(m.0.pext(Morton3::Z_MASK)),
+            untranslate(x_t_high_11 | x_t_low_21),
+            untranslate(y_t_high_11 | y_t_low_21),
+            untranslate(z_t_high_11 | z_t_low_21),
         ])
     }
+}
+
+/// Send the supported range of i32 into the lower 32 bits of a u64 while preserving the total order.
+#[inline]
+fn translate(x: i32) -> u64 {
+    x.wrapping_sub(i32::MIN) as u64
+}
+
+/// The inverse of `translate`.
+#[inline]
+fn untranslate(x: u64) -> i32 {
+    (x as i32).wrapping_add(i32::MIN)
 }
 
 // ████████╗███████╗███████╗████████╗
@@ -151,6 +151,18 @@ impl From<Morton3> for Point3i {
 mod test {
     use super::*;
     use crate::PointN;
+
+    #[test]
+    fn limits_of_i32() {
+        let min = PointN([i32::MIN; 3]);
+        let max = PointN([i32::MAX; 3]);
+
+        assert_eq!(Morton3::from(min), Morton3(0));
+        assert_eq!(Morton3::from(max), Morton3((1 << 96) - 1));
+
+        assert_eq!(min, Point3i::from(Morton3::from(min)));
+        assert_eq!(max, Point3i::from(Morton3::from(max)));
+    }
 
     #[test]
     fn octants_are_contiguous_in_morton_space() {
@@ -188,7 +200,7 @@ mod test {
 
     fn mortons_are_contiguous(mortons: &[Morton3]) -> bool {
         let min = mortons[0].0;
-        let upper_bound = min + mortons.len() as u64;
+        let upper_bound = min + mortons.len() as u128;
         for (i, expected) in (min..upper_bound).enumerate() {
             if mortons[i] != Morton3(expected) {
                 return false;

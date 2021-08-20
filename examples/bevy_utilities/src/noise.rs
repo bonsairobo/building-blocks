68142d5ec052838aa4478bb4d5772116c0a2270b
Robert Swain <robert.swain@gmail.com>
2021-08-21 00:07:44 +0200

    bevy_utilities: Address review feedback

--- examples/bevy_utilities/src/noise.rs
+++ examples/bevy_utilities/src/noise.rs
@@ -30,16 +30,13 @@ pub fn generate_noise_chunks2(
 }
 
 pub fn generate_noise_chunk3(
-    chunk_min: Point3i,
-    chunk_shape: Point3i,
+    chunk_extent: Extent3i,
     freq: f32,
     scale: f32,
     seed: i32,
     octaves: u8,
     subsurface_only: bool,
 ) -> Option<Array3x1<f32>> {
-    let chunk_extent = Extent3i::from_min_and_shape(chunk_min, chunk_shape);
-
     let mut array = noise_array3(chunk_extent, freq, seed, octaves);
 
     if subsurface_only {
@@ -80,7 +77,8 @@ pub fn generate_noise_chunks3(
         for p in chunks_extent.0.iter_points() {
             s.spawn(async move {
                 let chunk_min = p * chunk_shape;
-                generate_noise_chunk3(chunk_min, chunk_shape, freq, scale, seed, octaves, subsurface_only).and_then(|chunk| Some((chunk_min, chunk)))
+                let chunk_extent = Extent3i::from_min_and_shape(chunk_min, chunk_shape);
+                generate_noise_chunk3(chunk_extent, freq, scale, seed, octaves, subsurface_only).map(|chunk| (chunk_min, chunk))
             });
         }
     }).into_iter().filter_map(|x| x).collect()
